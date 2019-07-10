2cc125ab41c58e6efbc045ed4651c23ce0515375
MohammadJozaa7 <m.jozaa@yahoo.com>
2019-07-10 09:38:57 +0300

    upload the question

---
+++ quiz.js
@@ -0,0 +1,81 @@
+/*
+Q1:
+Write a function called isMyAgeAndExpEnough
+that takes two parameters an age and years of experince
+
+1- if your age grater than or equal to 25 and less than 30
+and the exp grater than or equal to 5
+return "you are awesome"
+
+2- if your age grater than or equal to 30
+and the exp grater than or equal to 5
+return "you are goood"
+
+3- if your age grater than or equal to 25
+and the exp less than 5 and grater than 2
+return "dont worry you need to work on your self more "
+
+4- if the age less than or equal to 24
+return "you still young"
+
+5- if the exp less than or equal to 2
+return "you need more exp"
+
+isMyAgeAndExpEnough(27,6)
+=>"you are awesome"
+
+isMyAgeAndExpEnough(35,6)
+=>"you are goood"
+
+isMyAgeAndExpEnough(26,3)
+=>"dont worry you need to work on your self more"
+
+isMyAgeAndExpEnough(22,1)
+=>"you still young"
+
+isMyAgeAndExpEnough(35,1)
+=>"you need more exp"
+*/
+
+
+
+
+
+
+
+
+/*
+Q2:
+Write a function called countDown
+that takes one parameter
+and return a string represent the count down from the number
+that insert to 0
+instead of 0 need to ahve "done"
+between each number a comma and space
+** you need to use recursion
+
+countDown(5)
+=> "5, 4, 3, 2, 1, done"
+
+countDown(2)
+=> "2, 1, done"
+
+countDown(7)
+=> "7, 6, 5, 4, 3, 2, 1, done"
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
